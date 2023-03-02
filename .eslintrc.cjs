71654c5f78879e267f0ba1d36307c7837f4ec4ec
arkteck <arkteck@gmail.com>
2023-03-02 09:50:43 -0500

    Setting up server and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  ignorePatterns: ['/dist/*']
+};
